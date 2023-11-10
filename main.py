0965daa2f92e52d2c5f0d6eb796d729675c3535a
chadlohrli <chad.david.lohrli@gmail.com>
2023-11-10 00:09:58 -0800

    add assistant.py and thread.py

--- main.py
+++ main.py
@@ -1,90 +1,75 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 from enum import Enum
-from typing import List, Dict, Union, Any, Optional
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import json
-import os
+import asyncio
+
+from assistant import Assistant, AssistantParams, Tool, ToolType
+from thread import Thread
 
 load_dotenv()
 
-client = OpenAI()
+client = AsyncOpenAI()
 
-class Function(BaseModel):
-    name: str
-    description: str
-    parameters: Dict[Any, Any]
+### sample functions
+def addTwo(num_one: int, num_two: int) -> int:
+  """
+  Add two numbers together.
 
-class ToolType(str, Enum):
-    function = "function"
-    code_interpreter = "code_interpreter"
-    retrieval = "retrieval"
+  Args:
+      num_one (int): The first number to add.
+      num_two (int): The second number to add.
 
-class Tool(BaseModel):
-    type: ToolType
-    function: Optional[Dict]
-    
-class AssistantParams(BaseModel):
-    model: str = Field(..., description="ID of the model to use.")
-    name: Optional[str] = Field(None, max_length=256, description="The name of the assistant.")
-    description: Optional[str] = Field(None, max_length=512, description="The description of the assistant.")
-    instructions: Optional[str] = Field(None, max_length=32768, description="The system instructions that the assistant uses.")
-    tools: Optional[List[Tool]] = Field(None, description="A list of tool enabled on the assistant.")
-    file_ids: Optional[List[str]] = Field(None, description="A list of File IDs attached to this assistant.")
-    metadata: Optional[dict] = Field(None, description="Set of 16 key-value pairs that can be attached to an object.")
+  Returns:
+      int: The sum of num_one and num_two.
+  """
+  return num_one + num_two
 
-def create_assistant(params: AssistantParams):
-    tools=[{'type': tool.type.value, 'function': tool.function} for tool in params.tools] if params.tools else []
-    print(tools)
-    assistant = client.beta.assistants.create(
-        name=params.name if params.name else "",
-        description=params.description if params.description else "",
-        instructions=params.instructions if params.instructions else "",
-        tools=[{'type': tool.type.value, 'function': tool.function} for tool in params.tools] if params.tools else [],
-        model=params.model if params.model else "gpt-4-1106-preview",
-        file_ids=params.file_ids if params.file_ids else [],
-        metadata=params.metadata if params.metadata else {}
-    )
-    return assistant
+async def create_root_assistant(assistant: Assistant):
+  with open('assistant_function.json', 'r') as f:
+    assistant_function = json.load(f)
 
-def create_root_assistant():
-    with open('assistant_function.json', 'r') as f:
-      assistant_function = json.load(f)
+  params = AssistantParams(
+    model="gpt-4-1106-preview",
+    name="Assistant Inception",
+    description="An Assistant that creates more Assistant",
+    instructions="Your job is to create more OpenAI Assistants. You can use your function calling tool to create assistants. Default to gpt-4-1106-preview as the model if not specified.",
+    tools=[Tool(type=ToolType.function, function=assistant_function)],
+  )
 
-    params = AssistantParams(
-      model="gpt-4-1106-preview",
-      name="Assistant Inception",
-      description="An Assistant that creates more Assistant",
-      instructions="Your job is to create more OpenAI Assistants. You can use your function calling tool to create assistants. Default to gpt-4-1106-preview as the model if not specified.",
-      tools=[Tool(type=ToolType.function, function=assistant_function)],
-      )
+  assistant = await assistant.create_assistant(params)
+  return assistant
 
-    assistant = create_assistant(params)
-    print(assistant)
 
-def create_assistant_thread():
-    thread = client.beta.threads.create(
-        messages = [
-            {
-                "role": "user",
-                "content": "please create a new assistant that can give me live weather reports. You may need to implement a function that can call the correct api"
-            }
-        ]
-    )
-    print(thread)
-    return thread
+async def create_simple_assistant(assistant: Assistant):
+  with open('simple_function.json', 'r') as f:
+    assistant_function = json.load(f)
 
-def invoke_root_assistant():
-    thread = create_assistant_thread()
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id="asst_VlCwS6nWjKERsar8Y6nnTMRh"
-    )
-    print(run)
-    return run
+  params = AssistantParams(
+    model="gpt-4-1106-preview",
+    name="Function Test",
+    description="An that adds nuumbers",
+    instructions="You add numbers together",
+    tools=[Tool(type=ToolType.function, function=assistant_function)],
+  )
 
+  assistant = await assistant.create_assistant(params)
+  return assistant
+
+async def main():
+  #assistant = Assistant(client)
+  #simple_assistant = await create_simple_assistant(assistant)
+  #print(simple_assistant)
+  
+  functions = {"addTwo": addTwo}
+  thread = Thread(client, "asst_vOQzvZ6gLNOPYnUyxgI98WWY", functions=functions) # simple assistant "asst_vOQzvZ6gLNOPYnUyxgI98WWY"
+  await thread.create_thread()
+  await thread.create_user_message("what's 8+9?")
+  await thread.run_thread("You are a personal math tutor. When asked a question, write and run Python code to answer the question.")
+  messages = await thread.get_messages()
+  print(messages)
+  
 if __name__ == "__main__":
-    run = invoke_root_assistant()
-
-
-     
+  asyncio.run(main())
+  
